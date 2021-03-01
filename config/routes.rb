7b0630cd4170125e46e74b834b7b9c7e3e31a281
catprice444 <catprice444@gmail.com>
2021-03-01 07:07:47 -0500

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get '/' => 'users#home'
+  get '/signup' => 'users#new'
+  
+  
   resources :items
   resources :schools
   resources :users
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
