dd3c08bea7c00aa55791029bf96de70563dd87ec
catprice444 <catprice444@gmail.com>
2021-03-02 06:11:53 -0500

    basic singup page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user
-    before_action :require_login
+   
 
     def current_user 
         @current_user ||= User.find_by(id: session[:user_id])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,23 +6,29 @@ class UsersController < ApplicationController
 
     def create 
         @user = User.create(user_params)
+        redirect_to user_path(@user)
     end 
 
     def show 
-        @user = User.find(params[:id])
+        @user = User.find_by(params[:id])
+        if @user.role == 1
+            render 'users/teachers/show'
+        else 
+            render 'users/donors/show'
+        end 
     end 
 
     def edit 
-        @user = User.find(params[:id])
+        @user = User.find_by(params[:id])
     end 
 
     def update 
-        @user = User.find(params[:id])
+        @user = User.find_by(params[:id])
     end 
 
     private 
     def user_params
-        params.require(:user).permit(:name, :password, :role, :donation_amount)
+        params.require(:user).permit(:name, :password, :role, )
     end 
 
     #should I create two different types of params, one for donor and one for teacher?
