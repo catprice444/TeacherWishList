2bb1ad74ae1852e747e856df2e51cc18baae7bfb
catprice444 <catprice444@gmail.com>
2021-03-02 06:48:07 -0500

    logout button works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,13 @@ class SessionsController < ApplicationController
     end
     
     def create
-        @user = User.find_by(name, params[:name])
-        redirect_to user_path(@user)
+        @user = User.find_by(name: params[:name])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to root_path
+        end
     end
     
     def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
 
-  delete '/logout' => 'sessions#destroy'
+  post '/logout' => 'sessions#destroy'
   
   
   resources :items
