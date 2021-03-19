78dfb824f40a0379f8568e22246082c2ff26ebfd
catprice444 <catprice444@gmail.com>
2021-03-19 06:30:45 -0400

    updating validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :school
-  validates :cost, presence: true
+  validates :cost, :name, presence: true
   validates :amount_needed, presence: true, on: [:create, :edit]
-  validates :name, presence: true
   scope :item_order, -> {order(:name)}
    
 
