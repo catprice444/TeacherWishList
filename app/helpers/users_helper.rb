61b7fe322d05349a37eecba8f397692bbb3a771a
catprice444 <catprice444@gmail.com>
2021-03-20 13:26:49 -0400

    user helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,12 @@
 module UsersHelper
    
+    def teacher_display
+        if  current_user.items.empty? 
+            tag.h3 ("Welcome! Head over to the School Home page to start requesting items")
+        else 
+            tag.h3 ("Requested items")
+        end 
+    end 
+
     
 end
