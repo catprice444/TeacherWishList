565364d7fa7ab7db26256acdacfba132819985ea
catprice444 <catprice444@gmail.com>
2021-03-20 13:02:35 -0400

    added school helpers

--- app/helpers/schools_helper.rb
+++ app/helpers/schools_helper.rb
@@ -1,2 +1,15 @@
 module SchoolsHelper
+    def school_index_page_links
+        if current_user.role == 1
+            tag.p(link_to('New School', new_school_path))
+        end 
+    end 
+
+    def school_show_page_links
+        if current_user.role == 1
+            tag.p(link_to "Add Item", new_school_item_path(@school))
+        else 
+        tag.p(Click on the item to donate)
+        end 
+    end 
 end
