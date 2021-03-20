015364fd7927b835c88b579af171f7a93bf6f5bc
catprice444 <catprice444@gmail.com>
2021-03-20 18:00:20 -0400

    updated background color

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,7 +2,7 @@ class School < ApplicationRecord
     has_many :items 
     has_many :users, through: :items
     
-    validates :name, presence: true, uniqueness: {message: "This school already exists"}
+    validates :name, presence: true, uniqueness: true
     validates :location, presence: true
 
     scope :school_order, -> {order(:name)}
