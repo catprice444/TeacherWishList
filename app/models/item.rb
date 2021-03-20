03a2ba1a82d39fb10e5ce9789eceb2e8f188f100
catprice444 <catprice444@gmail.com>
2021-03-20 11:58:03 -0400

    updating views

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :school
-  validates :cost, :name, presence: true
-  validates :amount_needed, presence: true, on: [:create, :edit]
+  
   scope :item_order, -> {order(:name)}
-   
+  
+  validates :cost, :name, presence: true
 
   def total_cost
     cost * amount_needed
--- app/models/school.rb
+++ app/models/school.rb
@@ -2,6 +2,6 @@ class School < ApplicationRecord
     has_many :items 
     has_many :users, through: :items
     scope :school_order, -> {order(:name)}
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: {message: "This school already exists"}
     validates :location, presence: true
 end
